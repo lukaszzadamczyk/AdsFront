9302dad49e320ac59f5585f29774df19231b3ece
Lukasz Adamczyk <lukaszz.adamczyk@gmail.com>
2022-05-13 11:13:49 +0100

    Leaflet maps are integrated.

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
+import '../../utils/fix-map-icon';
 
+import 'leaflet/dist/leaflet.css';
 import './Map.css';
 
 export const Map = () => {
     return(
-        <div className='map'>...</div>
+        <div className='map'>
+            <MapContainer center={[41.3808959,2.1183351]} zoom={13}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> & contributors"
+                />
+                <Marker position={[41.3808959,2.1183351]}>
+                    <Popup>
+                        <h2>Camp Nou</h2>
+                        <p>The biggest stadium in Europe.</p>
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </div>
     )
 }
